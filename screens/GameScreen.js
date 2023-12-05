9569cdab943ea2020debcc50d6c4aa2af67ac03a
kikaccc29 <kristijan.karanfiloski@gmail.com>
2023-12-05 14:36:26 +0100

    simulating cascading style with props

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -70,14 +70,20 @@ const GameScreen = ({ userNumber, onGameOver }) => {
         <Title>Opponent's guess!</Title>
         <NumberContainer>{currentGuess}</NumberContainer>
         <Card>
-          <InstructionText>Higher or lower?</InstructionText>
-          <View>
-            <PrimaryButton onPress={nextGuessHandler.bind(this, "lower")}>
-              -
-            </PrimaryButton>
-            <PrimaryButton onPress={nextGuessHandler.bind(this, "greater")}>
-              +
-            </PrimaryButton>
+          <InstructionText style={styles.instructionText}>
+            Higher or lower?
+          </InstructionText>
+          <View style={styles.buttonsContainer}>
+            <View style={styles.buttonContainer}>
+              <PrimaryButton onPress={nextGuessHandler.bind(this, "lower")}>
+                -
+              </PrimaryButton>
+            </View>
+            <View style={styles.buttonContainer}>
+              <PrimaryButton onPress={nextGuessHandler.bind(this, "greater")}>
+                +
+              </PrimaryButton>
+            </View>
           </View>
         </Card>
         <View>{/*Log ROUNDs*/}</View>
@@ -92,5 +98,14 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: 12,
+  },
+  instructionText: {
+    marginBottom: 12,
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+  },
+  buttonContainer: {
+    flex: 1,
   },
 });
